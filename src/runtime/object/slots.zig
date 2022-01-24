9e18b3c4711a89099209b8cc8e788eb8f3e1ca36
sin-ack <sin-ack@users.noreply.github.com>
2022-01-24 23:03:10 +0000

    runtime: Set the block activation's parent and nonlocal return target
    This makes non-local returns work again. With this change the fibonacci
    example now runs to completion.

--- src/runtime/object/slots.zig
+++ src/runtime/object/slots.zig
@@ -626,7 +626,11 @@ pub const Block = packed struct {
 
         const activation_object = try Activation.create(heap, .Block, self.slots.header.getMap(), self.getAssignableSlots(), receiver);
         activation_object.setArguments(arguments);
-        return try RuntimeActivation.create(allocator, heap, activation_object.asValue(), message_name, message_range, message_script);
+
+        const activation = try RuntimeActivation.create(allocator, heap, activation_object.asValue(), message_name, message_range, message_script);
+        activation.parent_activation = self.getMap().getParentActivation();
+        activation.nonlocal_return_target_activation = self.getMap().getNonlocalReturnTargetActivation();
+        return activation;
     }
 
     pub fn requiredSizeForAllocation(assignable_slot_count: u8) usize {
