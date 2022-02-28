06bff28e3a7d42943b2b708aa3a845ba22303566
sin-ack <sin-ack@users.noreply.github.com>
2022-02-28 13:37:41 +0000

    runtime: Reduce duplicate code in integer primitives

--- src/runtime/primitives/number.zig
+++ src/runtime/primitives/number.zig
@@ -13,74 +13,86 @@ const PrimitiveContext = @import("../primitives.zig").PrimitiveContext;
 
 // FIXME: Add overflow checks here
 
-/// Add two integer numbers. The returned value is an integer.
-pub fn IntAdd(context: PrimitiveContext) !Completion {
+fn integerOpCommon(
+    comptime primitive_name: [*:0]const u8,
+    context: PrimitiveContext,
+    operation: fn (receiver: i64, term: i64) Value,
+) !Completion {
     const receiver = context.receiver.getValue();
     const term = context.arguments[0].getValue();
 
     if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntAdd: receiver", .{});
+        return Completion.initRuntimeError(
+            context.interpreter_context.allocator,
+            context.source_range,
+            "Expected integer as _" ++ primitive_name ++ ": receiver",
+            .{},
+        );
     }
 
     if (!term.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntAdd: argument", .{});
+        return Completion.initRuntimeError(
+            context.interpreter_context.allocator,
+            context.source_range,
+            "Expected integer as _" ++ primitive_name ++ ": argument",
+            .{},
+        );
     }
 
-    return Completion.initNormal(Value.fromInteger(receiver.asInteger() + term.asInteger()));
+    return Completion.initNormal(operation(receiver.asInteger(), term.asInteger()));
+}
+
+/// Add two integer numbers. The returned value is an integer.
+pub fn IntAdd(context: PrimitiveContext) !Completion {
+    return integerOpCommon("IntAdd", context, struct {
+        pub fn op(receiver: i64, term: i64) Value {
+            return Value.fromInteger(receiver + term);
+        }
+    }.op);
 }
 
 /// Subtract the argument from the receiver. The returned value is an integer.
 pub fn IntSub(context: PrimitiveContext) !Completion {
-    const receiver = context.receiver.getValue();
-    const term = context.arguments[0].getValue();
-
-    if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntSub: receiver", .{});
-    }
-
-    if (!term.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntSub: argument", .{});
-    }
-
-    return Completion.initNormal(Value.fromInteger(receiver.asInteger() - term.asInteger()));
+    return integerOpCommon("IntSub", context, struct {
+        pub fn op(receiver: i64, term: i64) Value {
+            return Value.fromInteger(receiver - term);
+        }
+    }.op);
 }
 
 /// Multiply the argument with the receiver. The returned value is an integer.
 pub fn IntMul(context: PrimitiveContext) !Completion {
-    const receiver = context.receiver.getValue();
-    const term = context.arguments[0].getValue();
-
-    if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntMul: receiver", .{});
-    }
-
-    if (!term.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntMul: argument", .{});
-    }
-
-    return Completion.initNormal(Value.fromInteger(receiver.asInteger() * term.asInteger()));
+    return integerOpCommon("IntMul", context, struct {
+        pub fn op(receiver: i64, term: i64) Value {
+            return Value.fromInteger(receiver * term);
+        }
+    }.op);
 }
 
 /// Return whether the receiver is less than its argument. The return value is
 /// either "true" or "false".
 pub fn IntLT(context: PrimitiveContext) !Completion {
-    const receiver = context.receiver.getValue();
-    const term = context.arguments[0].getValue();
+    return integerOpCommon("IntLT", context, struct {
+        pub fn op(receiver: i64, term: i64) Value {
+            return if (receiver < term)
+                environment.globalTrue()
+            else
+                environment.globalFalse();
+        }
+    }.op);
+}
 
-    if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntLT: receiver", .{});
-    }
-
-    if (!term.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntLT: argument", .{});
-    }
-
-    return Completion.initNormal(
-        if (receiver.asInteger() < term.asInteger())
-            environment.globalTrue()
-        else
-            environment.globalFalse(),
-    );
+/// Return whether the receiver is greater than its argument. The return value
+/// is either "true" or "false".
+pub fn IntGT(context: PrimitiveContext) !Completion {
+    return integerOpCommon("IntGT", context, struct {
+        pub fn op(receiver: i64, term: i64) Value {
+            return if (receiver > term)
+                environment.globalTrue()
+            else
+                environment.globalFalse();
+        }
+    }.op);
 }
 
 /// Return whether the receiver is equal to its argument. The return value is
@@ -91,7 +103,12 @@ pub fn IntEq(context: PrimitiveContext) !Completion {
     const term = context.arguments[0].getValue();
 
     if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntEq: receiver", .{});
+        return Completion.initRuntimeError(
+            context.interpreter_context.allocator,
+            context.source_range,
+            "Expected integer as _IntEq: receiver",
+            .{},
+        );
     }
 
     if (!term.isInteger()) {
@@ -100,28 +117,6 @@ pub fn IntEq(context: PrimitiveContext) !Completion {
 
     return Completion.initNormal(
         if (receiver.asInteger() == term.asInteger())
-            environment.globalTrue()
-        else
-            environment.globalFalse(),
-    );
-}
-
-/// Return whether the receiver is greater than its argument. The return value
-/// is either "true" or "false".
-pub fn IntGT(context: PrimitiveContext) !Completion {
-    const receiver = context.receiver.getValue();
-    const term = context.arguments[0].getValue();
-
-    if (!receiver.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntGT: receiver", .{});
-    }
-
-    if (!term.isInteger()) {
-        return Completion.initRuntimeError(context.interpreter_context.allocator, context.source_range, "Expected integer as _IntGT: argument", .{});
-    }
-
-    return Completion.initNormal(
-        if (receiver.asInteger() > term.asInteger())
             environment.globalTrue()
         else
             environment.globalFalse(),
