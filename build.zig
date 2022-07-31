142045ce09395ec41f73c6588b4b35ddfb8520c3
sin-ack <sin-ack@users.noreply.github.com>
2022-07-31 10:32:42 +0000

    build: Revert musl default ABI target
    This causes build failure on non-Linux systems. People wishing to make
    static builds can still do `-Dtarget native-native-musl`.
    
    Fixes #11.

--- build.zig
+++ build.zig
@@ -10,9 +10,7 @@ pub fn build(b: *std.build.Builder) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{
-        .default_target = std.zig.CrossTarget{ .abi = .musl },
-    });
+    const target = b.standardTargetOptions(.{});
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
